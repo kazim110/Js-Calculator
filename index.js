a68cfb24343b11ac84aa7f5e150bfb0e43610222
Kazim Mohammadi <kazim.mohammadi110@gmail.com>
2022-02-22 14:18:02 +0430

    configuring js func to our calc

--- index.js
+++ index.js
@@ -1,43 +1,77 @@
-const numberButtons=document.querySelectorAll("[data-number]");
-const operatorButtons=document.querySelectorAll("[data-operator]");
-const equalButton=document.querySelector("[data-equals]");
-const allClearButton=document.querySelector("[data-all-clear]");
-const deleteButton=document.querySelector("[data-delete]");
-const prevOperandTextEl=document.querySelector("[data-prev-operand]");
-const currOperandTextEl=document.querySelector("[data-curr-operand]");
-
-
-class Calculator{
-    constructor(prevOperandTextEl,currOperandTextEl){
-        this.prevOperandTextEl=prevOperandTextEl;
-        this.currOperandTextEl=currOperandTextEl;
-        this.clear();
+function getHistory(){
+    return document.getElementById("history-value").innerText;
+}
+function printHistory(num){
+    document.getElementById("history-value").innerText=num;
+}
+function getOutput(){
+    return document.getElementById("output-value").innerText;
+}
+function printOutput(num){
+    if(num=""){
+        document.getElementById("output-value").innerText=num;
+    }else{
+        document.getElementById("output-value").innerText=getFormattedNumber(num);
     }
-
+}
+function getFormattedNumber(num){
+    if(num=="-"){
+        return "";
+    }
+    var n =number(num);
+    var value=n.toLocaleString("en");
+    return value;
+}
+function reverseNumberFormat(num){
+    return Number(num.replace(/,/g,''));
 }
 
-function clear(){
-    this.currOperandTextEl=''
-    this.prevOperandTextEl=''
-    this.operation=undefined
+var operator=document.getElementsByClassName("operator");
+for(var i=0;i<operator.length;i++){
+    operator[i].addEventListener('click',function(){
+        if(this.id=="clear"){
+            printHistory("");
+            printOutput("");
+        }
+        else if(this.id=="backspace"){
+            var output=reverseNumberFormat(getOutput()).toString();
+            if(output){
+                output=output.substring(0,output,length-1);
+                printOutput(output);
+            }
+        }
+        else{
+            var output=getOutput();
+            var history=getHistory();
+            if(output==""&history!=""){
+                if(isNaN(history[history.length-1])){
+                    history=history.substring(0,history,length-1);
+                }
+            }
+            if(output!=""||history!=""){
+                output=output==""?output:reverseNumberFormat(output);
+                history=history+output;
+                if(this.id=="="){
+                    var result=eval(history);
+                    printOutput(result);
+                    printHistory("");
+                }
+                else{
+                    history=history+this.id;
+                    printHistory(history);
+                    printOutput("");
+                }
+            }
+        }
+    });
 }
-delete(){
-
+var number =document.getElementsByClassName("number");
+for(var i=0;i<number.length;i++){
+    number[i].addEventListener('click',function(){
+        var output=reverseNumberFormat(getOutput());
+        if(output!=NaN){
+            output=output+this.id;
+            printOutput(output);
+        }
+    });
 }
-
-function appendNumber(number){
-
-}
-
-function chooseOperation(operation){
-
-}
-
-function compute(){
-
-}
-
-function updateDispley(){
-
-}
-
